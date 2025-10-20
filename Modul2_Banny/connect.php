bf802aca1b5eeb34b83aa0956ad24d455128e246
Tanuki <bannyjuanda@gmail.com>
2025-10-20 15:25:04 +0700

    Praktikum PAW Modul 2

---
+++ Modul2_Banny/connect.php
@@ -0,0 +1,24 @@
+<?php
+// host
+$host = "localhost";
+
+// username
+$username = "root";
+
+// password
+$pass = "";
+
+// database name
+$db = "modul_2";
+
+// port
+$port = 3307;
+
+// connect to db
+$conn = mysqli_connect($host, $username, $pass, $db, $port);
+
+// if cant connect
+if ($conn->connect_error) {
+  die("Koneksi Gagal: " . $koneksi->connect_error);
+}
+?>
